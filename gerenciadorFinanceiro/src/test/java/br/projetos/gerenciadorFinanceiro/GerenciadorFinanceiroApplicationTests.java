40b61015a2d6e096f23d9e055565719677cdf0da
Bruno <bruno.gmouro@gmail.com>
2023-10-21 18:02:41 -0300

    First Commit

---
+++ gerenciadorFinanceiro/src/test/java/br/projetos/gerenciadorFinanceiro/GerenciadorFinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.projetos.gerenciadorFinanceiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorFinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
